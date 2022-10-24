40309a810f64a4d974239d6377a720f79aedb045
Calupator <calupator@mail.ru>
2022-10-24 05:37:49 +0300

    DZ-6

---
+++ Task41/Program.cs
@@ -0,0 +1,25 @@
+﻿// =====================================================================================================================
+//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> (3)4!!!
+
+static void CreateArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+Console.WriteLine("Введите последовательно элементы массива:");
+CreateArray(array);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+        count++;
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine("Кол-во положительных чисел: " + count);
---
+++ Task43/Program.cs
@@ -0,0 +1,18 @@
+﻿// =====================================================================================================================
+//Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых,
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+double[] LinesIntersects(double a, double c, double b, double d)
+{
+    double[] retVal = new double[2];
+    retVal[0] = (d - c) / (a - b);
+    retVal[1] = ((a * d) - (b * c)) / (a - b);
+    return retVal;
+}
+
+Console.Clear();
+
+double b1 = 2, k1 = 5;
+double b2 = 4, k2 = 9;
+double[] point = LinesIntersects(k1, b1, k2, b2);
+Console.Write($"Точка пересечения двух прямых: ({string.Join(", ", point)})");
