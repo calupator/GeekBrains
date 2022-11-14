7b2b128249cf679e5239ba62c40363569e6fe95b
Calupator <calupator@mail.ru>
2022-11-14 18:42:29 +0300

    DZ-8

---
+++ Task54/Program.cs
@@ -0,0 +1,63 @@
+﻿// =====================================================================================================================
+// Задача 54. Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void CreateArray(double[,] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            //arr[i, j] = Math.Round(rnd.NextDouble() * 10, 2);
+            arr[i, j] = rnd.Next(-9, 10);
+            if (rnd.Next(0, 2) == 1)
+                arr[i, j] *= (-1);
+        }
+    }
+}
+
+void PrintArray(double[,] arr)
+{
+    Console.WriteLine();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write(arr[i, j] + " \t");
+        Console.WriteLine();
+    }
+}
+
+void SortMatrix(double[,] arr)
+{
+    double temp = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = arr.GetLength(1) - 1; j > 0; j--)
+        {
+            for (int k = 0; k < j; k++)
+            {
+                if (arr[i, k] < arr[i, k + 1])
+                {
+                    temp = arr[i, k];
+                    arr[i, k] = arr[i, k + 1];
+                    arr[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+Console.Clear();
+
+Console.Write("Введите размерность массива через пробел: ");
+string[] num = Console.ReadLine().Split(' '); // ввод двух чисел в одну строку
+int n = int.Parse(num[0]);
+int m = int.Parse(num[1]);
+double[,] matr = new double[m, n];
+CreateArray(matr);
+Console.Write("Начальный массив: ");
+PrintArray(matr);
+SortMatrix(matr);
+Console.Write("Результат: ");
+PrintArray(matr);
+
---
+++ Task56/Program.cs
@@ -0,0 +1,55 @@
+﻿// =====================================================================================================================
+// Задача 56. Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.Clear();
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(0, 10);
+    }
+}
+
+void Print(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+    }
+}
+
+int CompareRow(int[,] matrix)
+{
+    int[] SumRow = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+       for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            SumRow[i] += matrix[i, j];
+        }
+    }
+    int min = SumRow[0];
+    int NumberRow = 0;
+    for (int i = 0; i < SumRow.Length; i++)
+    {
+        if (SumRow[i] < min)
+            min = SumRow[i];
+            NumberRow = i + 1;
+    }
+    return min;
+}
+
+Console.Clear();
+Console.Write("Введите размерность прямоугольного массива (n x n): n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+Console.WriteLine("Пев : ");
+InputMatrix(matrix);
+Print(matrix);
+Console.WriteLine ($"Минимальное значение суммы элементов строки: " + CompareRow(matrix));
