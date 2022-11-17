b79feab0043fe5fea14ac31d6d86de8bb96e554c
Calupator <calupator@mail.ru>
2022-11-17 19:50:18 +0300

    DZ-9 предыдущее DZ-8 сдам чуть позже

---
+++ Task66/Program.cs
@@ -0,0 +1,32 @@
+﻿// =====================================================================================================================
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//(НЕ ИСПОЛЬЗОВАТЬ РЕЗУЛЬТИРУЮЩУЮ ПЕРЕМЕННУЮ)
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int Sum(int x, int y)
+{
+    if (x == y)
+        return x;
+    return x + Sum(++x, y);
+}
+
+Console.Clear();
+
+Console.Write("Введите начало диапазона M = ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите конец диапазона N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (m > n)
+{
+    Console.WriteLine("Значение M не должно превышать значение N!!!");
+
+    Console.Write("Введите начало диапазона M = ");
+    m = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите конец диапазона N = ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("M = " + m + "; N = " + n + " -> " + Sum(m, n));
---
+++ Task68/Program.cs
@@ -0,0 +1,36 @@
+﻿// =====================================================================================================================
+//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+int Akkerman(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else if (n != 0 && m == 0)
+        return Akkerman(n - 1, 1);
+    else if (n > 0 && m > 0)
+        return Akkerman(n - 1, Akkerman(n, m - 1));
+    return Akkerman(n, m);
+}
+
+Console.Clear();
+
+Console.Write("Введите положительное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите положительное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while ((m < 0) || (n < 0))
+{
+    Console.WriteLine("Значения M и N должны быть положительными!!!");
+
+    Console.Write("Введите положительное число M: ");
+    m = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите положительное число N: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+Akkerman(n, m);
+Console.WriteLine("M = " + m + "; N = " + n + " -> " + Akkerman(m, n));
